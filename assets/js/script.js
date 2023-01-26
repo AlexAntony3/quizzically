5f16979ba0cc3d1cade917e2c695cc59678d58f2
AlexAntony3 <alex.antony.jy@gmail.com>
2023-01-26 18:41:55 +0000

    feat(js): add example questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,38 @@ const question = document.getElementById("question");
 const options = Array.from(document.getElementsByClassName("option"));
 
 let currentQuestion = [];
-let userAnswer = false;
+let userAnswer = true;
 let score = 0;
 let questionNumber = 0;
 let possibleQuestions = [];
 let difficultyLevel = '';
 
-let questions = [];
+let questions = [
+    {
+        question: "this is question number 1",
+        option1: "answer1",
+        option2: "answer2",
+        option3: "answer3",
+        option4: "answer4",
+        answer: 1
+    },
+    {
+        question: "this is question number 2",
+        option1: "answer1",
+        option2: "answer2",
+        option3: "answer3",
+        option4: "answer4",
+        answer: 2
+    },
+    {
+        question: "this is question number 3",
+        option1: "answer1",
+        option2: "answer2",
+        option3: "answer3",
+        option4: "answer4",
+        answer: 3
+    }
+];
 
 const maxQuestions = 10;
 
@@ -16,12 +41,20 @@ startQuiz = () => {
     questionNumber = 0;
     score = 0;
     possibleQuestions = [...questions];
+    console.log(possibleQuestions);
     genNewQuestion();
 };
 
 genNewQuestion = () => {
     questionNumber++;
     const questionList = Math.floor(Math.random() * possibleQuestions.length);
-    currentQuestion = possibleQuestions(questionList);
+    currentQuestion = possibleQuestions[questionList];
     question.innerText = currentQuestion.question;
-}
+
+    options.forEach( choice => {
+        const number = option.dataset['answer'];
+        option.innerText = currentQuestion['option' + number];
+    })
+};
+
+startQuiz();
