6f213669b2b037a9c572941297263d8185ab605b
AlexAntony3 <alex.antony.jy@gmail.com>
2023-01-26 18:28:19 +0000

    feat(js): add start quiz and new quiz generation function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 const question = document.getElementById("question");
-const options = document.getElementsByClassName("option");
+const options = Array.from(document.getElementsByClassName("option"));
 
 let currentQuestion = [];
 let userAnswer = false;
@@ -8,5 +8,20 @@ let questionNumber = 0;
 let possibleQuestions = [];
 let difficultyLevel = '';
 
+let questions = [];
 
+const maxQuestions = 10;
 
+startQuiz = () => {
+    questionNumber = 0;
+    score = 0;
+    possibleQuestions = [...questions];
+    genNewQuestion();
+};
+
+genNewQuestion = () => {
+    questionNumber++;
+    const questionList = Math.floor(Math.random() * possibleQuestions.length);
+    currentQuestion = possibleQuestions(questionList);
+    question.innerText = currentQuestion.question;
+}
