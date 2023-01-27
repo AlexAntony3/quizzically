e5c1bdfc547a31889e0f1528ddbb3f9517b47967
AlexAntony3 <alex.antony.jy@gmail.com>
2023-01-27 18:14:00 +0000

    fix(js): adjust answers and splice question to used questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,15 @@
 const question = document.getElementById("question");
 const options = Array.from(document.getElementsByClassName("option"));
+const easyQ = document.getElementById("easy")
+const mediumQ = document.getElementById("medium")
+const hardQ = document.getElementById("hard")
+
 
 let currentQuestion = [];
-let userAnswer = true;
+let userAnswer = false;
 let score = 0;
 let questionNumber = 0;
 let possibleQuestions = [];
-let difficultyLevel = '';
 
 let questions = [
     {
@@ -19,23 +22,25 @@ let questions = [
     },
     {
         question: "this is question number 2",
-        option1: "answer1",
-        option2: "answer2",
-        option3: "answer3",
-        option4: "answer4",
+        option1: "answer5",
+        option2: "answer6",
+        option3: "answer7",
+        option4: "answer8",
         answer: 2
     },
     {
         question: "this is question number 3",
-        option1: "answer1",
-        option2: "answer2",
-        option3: "answer3",
-        option4: "answer4",
+        option1: "answer9",
+        option2: "answer10",
+        option3: "answer11",
+        option4: "answer12",
         answer: 3
     }
 ];
 
+
 const maxQuestions = 10;
+
 
 startQuiz = () => {
     questionNumber = 0;
@@ -51,10 +56,18 @@ genNewQuestion = () => {
     currentQuestion = possibleQuestions[questionList];
     question.innerText = currentQuestion.question;
 
-    options.forEach( choice => {
-        const number = option.dataset['answer'];
-        option.innerText = currentQuestion['option' + number];
+    options.forEach( option => {
+        const answer = option.dataset['answer'];
+        option.innerText = currentQuestion['option' + answer];
     })
+
+    possibleQuestions.splice(questionList, 1);
+
+    acceptingAnswers = true;
 };
+
+options.forEach(choice => {
+
+})
 
 startQuiz();
